a8b873a7ca712a7520add22ee364ad195168e7c2
kentalabur <kl459@cornell.edu>
2015-04-15 18:42:41 -0400

    replaced cipher suite and used one recommended by NIST (http://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-52r1.pdf) instead

--- src/util/Consts.java
+++ src/util/Consts.java
@@ -3,5 +3,5 @@ package util;
 public class Consts{
 	public static final int SERVER_PORT = 8888;
 	public static final int LOGSERVER_PORT = 9999;
-	public static final String[] ACCEPTED_SUITES = new String[]{"TLS_RSA_WITH_AES_128_CBC_SHA256"};
+	public static final String[] ACCEPTED_SUITES = new String[]{"TLS_RSA_WITH_AES_128_GCM_SHA256"};
 }
