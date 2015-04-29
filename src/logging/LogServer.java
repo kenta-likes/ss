ffa3353402a94082669ca70111e293081b6c5cc2
Kyle Donahue <kyle.donahue@gmail.com>
2015-04-28 21:25:01 -0400

    Revised the current log server stuff

--- src/logging/LogServer.java
+++ src/logging/LogServer.java
@@ -5,40 +5,138 @@ import javax.net.ssl.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.security.*;
+
 import util.Consts.*;
+import util.Response;
+import javax.xml.bind.DatatypeConverter;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.spec.KeySpec;
 
 public class LogServer {
+
+    protected static SecretKey key;
+    protected static byte[] keyBytes;
+    protected static String HOSTNAME;
+    protected static String ADMIN_PASSWORD;
 
     public static void main(String[] args) {
         String ksName = "auditstore.jks"; //server side keystore
         char ksPass[] = "systemsecurity".toCharArray();
         char ctPass[] = "systemsecurity".toCharArray();
+
+        if (args.length < 1) {
+            System.out.println("error: did not receive hostname argument");
+            return;
+        }
+
+        /* To make sure we are connected to the right people... */
+        HOSTNAME = args[0];
+        
         try {
             KeyStore ks = KeyStore.getInstance("JKS");
             ks.load(new FileInputStream(ksName), ksPass);
-            KeyManagerFactory kmf = 
+            KeyManagerFactory kmf =
                 KeyManagerFactory.getInstance("SunX509");
             kmf.init(ks, ctPass);
             SSLContext sc = SSLContext.getInstance("TLS");
             sc.init(kmf.getKeyManagers(), null, new SecureRandom());
             SSLServerSocketFactory ssf = sc.getServerSocketFactory();
-            SSLServerSocket s 
+            SSLServerSocket s
                 = (SSLServerSocket) ssf.createServerSocket(8888);
 
             ExecutorService executor = Executors.newFixedThreadPool(8);
+
+            /* Read in key. */
+            BufferedReader f = new BufferedReader(new FileReader("logkey.conf"));
+            keyBytes = DatatypeConverter.parseBase64Binary(f.readLine());
+
+            SecretKeyFactory keyFact = SecretKeyFactory.getInstance("AES");
+            KeySpec spec = new SecretKeySpec(keyBytes, "AES/CBC/PKCS5Padding");
+            key = keyFact.generateSecret(spec);
+
+
             while (true) {
                 SSLSocket c = (SSLSocket) s.accept();
                 Runnable connection = new logging.LogConnection(c);
                 executor.execute(connection);
             }
-         
+
         } catch (Exception e) {
-            //System.err.println(e.toString());
             e.printStackTrace();
         }
     }
 
-    public static void log() {
-        
+    public static Response log(String entry, String tag) {
+
+        /* Check signature is OK. */
+        if (!authenticate(entry, tag))
+            return Response.FAIL;
+
+        /* Write line to disk. */
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("log.txt"));
+            writer.write(entry + "\t" + tag);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Response.FAIL;
+        }
+
+        iterateKey();
+        return Response.SUCCESS;
     }
+
+    protected static String getLog() {
+        failwith "Not implemented!"
+    }
+
+    private static boolean authenticate(String logLine, String tagLine) {
+        byte[] logBytes = DatatypeConverter.parseBase64Binary(logLine);
+        byte[] tag = DatatypeConverter.parseBase64Binary(tagLine);
+        byte[] tagFromEntry;
+        boolean equal = true;
+
+        try {
+            /* Re-MAC the message and check the two tags are equal. */
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(key);
+            tagFromEntry = mac.doFinal(logBytes);
+
+            if (tag.length != tagFromEntry.length)
+                return false;
+
+            for (int i = 0; i < tag.length; i++) {
+                equal &= (tag[i] == tagFromEntry[i]);
+            }
+
+            return equal;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    /* Hash key bytes with SHA-256 and update the key. */
+    private static void iterateKey() {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] digest = md.digest(keyBytes);
+
+            /* Use only first 256 bits of hash. */
+            keyBytes = java.util.Arrays.copyOf(digest, 32);
+
+            SecretKeyFactory keyFact = SecretKeyFactory.getInstance("AES");
+            KeySpec spec = new SecretKeySpec(keyBytes, "AES/CBC/PKCS5Padding");
+            key = keyFact.generateSecret(spec);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
